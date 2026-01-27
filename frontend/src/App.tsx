ef82929b85a288c0ea93857b9839b0ca6d86fc5d
Roman Romanenko <capital.car7@gmail.com>
2026-01-27 15:29:14 +0100

    Create app routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
 import "./styles/index.scss";
+import {Route, Routes} from "react-router-dom";
+import {APP_ROUTES} from "./system/router/contstans.ts";
 
 function App() {
-  return ( <p>Hello</p>)
+  return (
+      <Routes>
+          <Route path={APP_ROUTES.index} element={<div>Home</div>}/>
+      </Routes>
+  )
 }
 
 export default App
