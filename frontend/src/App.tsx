5774f9210c5746ed324c0f2605b2e729e03645af
Roman Romanenko <capital.car7@gmail.com>
2026-01-27 15:37:38 +0100

    Add Not Found Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
 import "./styles/index.scss";
 import {Route, Routes} from "react-router-dom";
 import {APP_ROUTES} from "./system/router/contstans.ts";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
       <Routes>
           <Route path={APP_ROUTES.index} element={<div>Home</div>}/>
+
+          {/* MUST be last */}
+          <Route path="*" element={<NotFound />} />
       </Routes>
   )
 }
