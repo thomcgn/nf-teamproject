bc2004e6abf6a075f6f9ceb3f21885ee69d01f6f
Roman Romanenko <capital.car7@gmail.com>
2026-01-29 18:48:06 +0100

    Move to Spring Boot v4

--- backend/src/test/java/com/example/backend/controller/RecipeControllerTest.java
+++ backend/src/test/java/com/example/backend/controller/RecipeControllerTest.java
@@ -8,10 +8,9 @@ import com.example.backend.services.RecipeService;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
@@ -26,7 +25,7 @@ class RecipeControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private RecipeService recipeService;
 
     @Test
