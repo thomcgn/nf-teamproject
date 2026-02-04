ee2c1393c39b31ab7bf73178192956d4446d12e3
sarav <sara.verre@outlook.it>
2026-02-04 14:13:54 +0100

    remove unused import

--- backend/src/main/java/com/example/backend/security/AuthController.java
+++ backend/src/main/java/com/example/backend/security/AuthController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
